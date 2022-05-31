07d76addf17413c623c4bd2b6263bfdaf4ad14e2
beargame123 <hoyoung7827@gmail.com>
2022-05-31 16:37:29 +0900

    💻 초기 세팅

---
+++ oauthProjects/src/test/java/com/example/oauthprojects/OauthProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauthprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthProjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
